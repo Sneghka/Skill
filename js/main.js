c94334384176707e9eb770c96ed0c0b10302aeb4
Sneghka <sneghkan@mail.ru>
2016-07-11 10:09:32 +0300

    *

--- js/main.js
+++ js/main.js
@@ -1,29 +1,29 @@
  $(document).ready(function () {
-     $('.bxslider').bxSlider({
-         mode: 'fade',
-         speed: 5000,
-         auto: true,
-         /* adaptiveHeight: false,*/
-         /*pager: false,
-         controls: false*/
+             $('.bxslider').bxSlider({
+                 mode: 'fade',
+                 speed: 5000,
+                 auto: true,
+                 /* adaptiveHeight: false,*/
+                 /*pager: false,
+                 controls: false*/
 
-     });
+             });
 
-     $(window).width(function () {
-         var widthW = $(window).width();
-         if(widthW > 799)
-         /*if(widthW > 1110 || ((widthW >=800 && widthW <= 1024)&&(window.orientation == 90 || window.orientation == -90) ))*/ {
-              $("#front_slider_1").attr("src", "images/Mc_front.png"),
-                 $("#front_slider_2").attr("src", "images/Univ_front.png"),
-                  $(".laptop").attr("src", "images/Laptop.png");
-         }
-         else  {
-             $("#front_slider_1").attr("src", "images/Mc_front_min.png"),
-                 $("#front_slider_2").attr("src", "images/Univer_front_min.png"),
-                 $(".laptop").attr("src", "images/Laptop_min.png");
-         }
-     });     
-    
- });
+             $(window).width(function () {
+                 var widthW = $(window).width();
+                 if (widthW > 799)
+                 /*if(widthW > 1110 || ((widthW >=800 && widthW <= 1024)&&(window.orientation == 90 || window.orientation == -90) ))*/
+                 {
+                     $("#front_slider_1").attr("src", "images/Mc_front.png"),
+                         $("#front_slider_2").attr("src", "images/Univer_front.png"),
+                         $(".laptop").attr("src", "images/Laptop.png");
+                 } else {
+                     $("#front_slider_1").attr("src", "images/Mc_front_min.png"),
+                         $("#front_slider_2").attr("src", "images/Univer_front_min.png"),
+                         $(".laptop").attr("src", "images/Laptop_min.png");
+                 }
+             });
 
+          /*  НАПИСАТЬ ФУНКЦИЮ ЗАМЕНЫ БЕКГРАУНДА В ЗАВИСИМОСТИ ОТ АТРИБУТОВ ССЫЛККИ*/
 
+             });
