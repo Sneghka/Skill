b64139ec1d811485d3857e8654b0d35c4ead2bf5
Sneghka <sneghkan@mail.ru>
2016-07-11 15:31:40 +0300

    add controls-direction and controls-pager

--- js/main.js
+++ js/main.js
@@ -1,29 +1,52 @@
  $(document).ready(function () {
-             $('.bxslider').bxSlider({
-                 mode: 'fade',
-                 speed: 5000,
-                 auto: true,
-                 /* adaptiveHeight: false,*/
-                 /*pager: false,
-                 controls: false*/
+     $('.bxslider').bxSlider({
+         mode: 'fade',
+         speed: 5000,
+         auto: true,
+         /* adaptiveHeight: false,*/
+         /*pager: false,
+         controls: false*/
 
-             });
+     });
 
-             $(window).width(function () {
-                 var widthW = $(window).width();
-                 if (widthW > 799)
-                 /*if(widthW > 1110 || ((widthW >=800 && widthW <= 1024)&&(window.orientation == 90 || window.orientation == -90) ))*/
-                 {
-                     $("#front_slider_1").attr("src", "images/Mc_front.png"),
-                         $("#front_slider_2").attr("src", "images/Univer_front.png"),
-                         $(".laptop").attr("src", "images/Laptop.png");
-                 } else {
-                     $("#front_slider_1").attr("src", "images/Mc_front_min.png"),
-                         $("#front_slider_2").attr("src", "images/Univer_front_min.png"),
-                         $(".laptop").attr("src", "images/Laptop_min.png");
-                 }
-             });
+     $(window).width(function () {
+         var widthW = $(window).width();
+         if (widthW > 799)
+         /*if(widthW > 1110 || ((widthW >=800 && widthW <= 1024)&&(window.orientation == 90 || window.orientation == -90) ))*/
+         {
+             $("#front_slider_1").attr("src", "images/Mc_front.png"),
+                 $("#front_slider_2").attr("src", "images/Univer_front.png"),
+                 $(".laptop").attr("src", "images/Laptop.png");
+         } else {
+             $("#front_slider_1").attr("src", "images/Mc_front_min.png"),
+                 $("#front_slider_2").attr("src", "images/Univer_front_min.png"),
+                 $(".laptop").attr("src", "images/Laptop_min.png");
+         }
+     });
 
-          /*  НАПИСАТЬ ФУНКЦИЮ ЗАМЕНЫ БЕКГРАУНДА В ЗАВИСИМОСТИ ОТ АТРИБУТОВ ССЫЛККИ*/
+     /*$(".bx-prev").css("href", "http://api.jquery.com/attr/")
+       var frontSliderHeigth = $(".front_slider").height();
+       var heightFromTopWindowToTopFrontSlider = $(".front_slider").offset().top;
+       var halfArrowheigth = 36;
+       var topHeightForArrow = Math.round((frontSliderHeigth / 2 + heightFromTopWindowToTopFrontSlider - halfArrowheigth) * 100 / window.innerHeight) + "%";
+       $(".bx-prev").css("top", "topHeightForArrow");
+      */
 
-             });
+ });
+
+ function ControlsDirection() {
+     var frontSliderHeigth = $(".front_slider").height();
+     var heightFromTopWindowToTopFrontSlider = $(".front_slider").offset().top;
+     var halfArrowheigth = 30;
+     var topHeightForArrow1 = Math.round((frontSliderHeigth / 2 + heightFromTopWindowToTopFrontSlider - halfArrowheigth) * 100 / window.innerHeight) + "%";
+     if(window.innerWidth > 799) {
+             $(".bx-prev").attr("style", "top:" + topHeightForArrow1);
+             $(".bx-next").attr("style", "top:" + topHeightForArrow1);
+         }
+     else {
+         
+     }
+         /* alert(frontSliderHeigth + " / " + heightFromTopWindowToTopFrontSlider + " / " + " / "+ screen.height + topHeightForArrow);*/
+
+
+ }
